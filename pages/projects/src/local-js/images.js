2e763cf01052918915ba6b7983baf96aa83d82a4
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-11 11:32:03 -0700

    setting variables for tech icons

---
+++ pages/projects/src/local-js/images.js
@@ -0,0 +1,19 @@
+// Define the image paths in a JavaScript object
+const imagePaths = {
+    graphQL_header: "./images/graphQLHeader.png",
+    python_icon: "./images/python.png",
+    apollo_icon: "./images/apollo.png",
+    cpp_icon: "./images/cpp.png",
+    nodeJs_icon: "./images/nodeJs.png",
+    graphQL_icon: "./images/graphQL.png"
+};
+
+// Set the src attributes of the img elements after the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('graphQL_header').src = imagePaths.graphQL_header;
+    document.getElementById('python_icon').src = imagePaths.python_icon;
+    document.getElementById('apollo_icon').src = imagePaths.apollo_icon;
+    document.getElementById('cpp_icon').src = imagePaths.cpp_icon;
+    document.getElementById('nodeJs_icon').src = imagePaths.nodeJs_icon;
+    document.getElementById('graphQL_icon').src = imagePaths.graphQL_icon;
+});
