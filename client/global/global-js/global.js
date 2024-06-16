4417ef5003ca7205d71b4a238c47c93157323453
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-15 22:04:33 -0700

    testing global script definitions

---
+++ client/global/global-js/global.js
@@ -0,0 +1,11 @@
+// EXTERNAL IMPORTS
+document.write('<script src="https://cdn.jsdelivr.net/npm/prismjs/prism.js"></script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>');
+
+// CUSTOM IMPORTS
+document.write('<script src="./cacheBust.js"></script>');
+document.write('<script src="./codeModal.js"></script>');
+document.write('<script src="./copyText.js"></script>');
+document.write('<script src="./darkMode.js"></script>');
+document.write('<script src="./linking.js"></script>');
+
